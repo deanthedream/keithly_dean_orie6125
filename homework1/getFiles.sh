7b3a93c9b469979eb8f867dd414295fba9dd01e9
Dean Keithly <drk94@cornell.edu>
2018-02-11 14:22:34 -0500

    a test of getting correct files into instance

--- homework1/getFiles.sh
+++ homework1/getFiles.sh
@@ -5,9 +5,31 @@
 #directory=taq.12.2014/
 #locate_dir=$'s3://'$directory
 #aws s3 ls locate_dir > fileNames.txt
-aws s3 ls s3://taq.12.2014/ > filenames.txt
-#s3cmd ls -r locate_dir | awk '{print $4}' > objects_in_bucket
+filename='filenames.txt'
+aws s3 ls s3://taq.12.2014/ > $filename
+
+#filelines=`cat $filename` #this gets the lines in filename
 
 
+head -n 7 $filename | sed -r 's/.*\s//' > filename2.txt #outputs first 7 lines of file to file
+echo 'done reducing to 7 filenames'
+
+filename2='filename2.txt'
+filelines2=`cat $filename2`
+
+
+for line in $filelines2 ; do #iterate through each line in filename
+   echo $line
+   locate_dir=$'s3://'$directory$line
+   sudo aws s3 cp $locate_dir ./temp.zip;
+   name_file="$(unzip -l temp.zip | awk '/-----/ {p = ++p % 2; next} p {print $NF}')";
+   unzip temp.zip;
+   rm -f temp.zip;
+   head -n 1000 $name_file  > tmp_file;
+   mv tmp_file $name_file
+done
 #physically grabs file from server
 #sudo aws s3 cp s3://taq.12.2014/$filename
+
+
+
