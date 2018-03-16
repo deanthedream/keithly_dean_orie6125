68380c14f58c4cfc0b8eb0535dd391b06cc0b98a
Dean Keithly <drk94@cornell.edu>
2018-03-16 18:42:43 -0400

    modification to make travis yml run

--- homework2/hmwk2PATHS.py
+++ homework2/hmwk2PATHS.py
@@ -6,7 +6,7 @@ import fnmatch
 files = [f for f in os.listdir('.') if os.path.isdir(f)] # Get dir in current dir
 files2 = [f for f in files if not '.' in f] # remove filename if there is a dot in it
 files3 = list()
-folderExclusion = ['tests','env']
+folderExclusion = ['tests','env','coverage','htmlcov']
 for folderName in files2:
     if not folderName in folderExclusion:
         files3.append(folderName)
