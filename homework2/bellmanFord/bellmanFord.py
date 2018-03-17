1eabf764c29037e6175a54c7cb5608e0d3b9b2c4
Dean Keithly <drk94@cornell.edu>
2018-03-17 05:34:55 -0400

    working bellman ford algorithm

--- homework2/bellmanFord/bellmanFord.py
+++ homework2/bellmanFord/bellmanFord.py
@@ -1,11 +1,24 @@
+import numpy as np
+import random
 
+
+NIL = None
 
 class graph(object):
 
-    def __init__(self,V,E):
-        self.V
-        self.E
+    def __init__(self,V,E,w):
+        self.V = list()
+        for i in np.arange(0,len(V)):
+            self.V.append(vertice(V[i]))
+        self.E = E#contains edge connections
+        self.w = w#contains weights of edges
 
+class vertice(object):
+
+    def __init__(self,val):
+        self.val = val
+        self.d = np.inf
+        self.pi = NIL
 
 class bellmanFord(object):
 
@@ -13,22 +26,78 @@ class bellmanFord(object):
         pass
 
     def init_single_source(self,G,s):
-        for i in range(0,G.V.shape[0]-1-1):  # Iterate over all vertices-1 *Results in have indices from [0,3] for G.V.shape[0]=5 *because last one has already been gone over
-            # V = G.V[G.V[i] != i]  # recasts vertices to look at
-            for j in range(0,G.E[i,:].shape[0]):  # for each edge (u,v) for all G.E
-                self.relax(u,v,w)
-        for  # for each edge (u,v) for all G.E
-            if v.d > u.d + w(u,v):
-                return False
-        return True
+        """
+        Args:
+            G - weighted directed Graph with vertices G.V and Weighted Edges G.E
+            s - source index G.V[s] is the source Vertice
+        """
+        for i in np.arange(len(G.V)):
+            G.V[i].d = np.inf#set all path from source to v to inf
+            G.V[i].pi = NIL
+        G.V[s].d = 0#The source vertice
+        return G, s
 
-    def relax(self,u,v,w):
-        if v.d > u.d + w(u,v):  # The distance from starting node s to current node v is longer than s to u + u to v
-            v.d = u.d + w(u,v)  # replace s to v with s to u + u to v
-            v.pi = u # update v's predecessor attribute pi
+    def bellman_ford(self, G,s):
+        """
+        Returns:
+            cycleExists - boolean indicating whether a negative cycle is reachable from the source (True) or not (False)
+        """
+        G, s = self.init_single_source(G,s)
+        w = G.w#w is the weighting vector
 
+        array = np.arange(len(G.V))  # This is the full array of vertice indices
+        #We need to iterate through all the nodes intelligently
+        seen = list()
+        tosee = list()
+        tosee.append(s)#np.array([s])
+        #for i in np.delete(array,s):  # Array does not include Node s
+        while len(tosee) > 0:#There is at least one node to use as u
+            i = tosee[0]
+            # for each edge (u,v) for all G.E
+            #for j in np.arange(len(G.E)):#Iterate through edges from u to next vertice. #np.arange(len(G.E)):
+            #for j in [G.E[k] for k in np.arange(len(G.E[:])) if G.E[k][0] == i][0]:
+            for j in [[G.E[k][1],k] for k in np.arange(len(G.E[:])) if G.E[k][0] == i]:
+                #for []:
+                #if G.E[j][0] == i:
+                #uv = G.E[j]
+                #u = uv[0]
+                #v = uv[1]
+                #if uv[0] == i:#I added this
+                u = i
+                v = j[0]
+                #print(v)
+                G = self.relax(G,u,v,w[j[1]])
 
-    def check_negative_cycles(self,):
-        # if v(s,n) < v(s,n-1):#for s to t, then negative cycles exist
-            # then negative cycles return false
-        pass
+                if not v in tosee and not v in seen:
+                    tosee.append(v)#add to list of vertices to observe
+                if not u in seen:
+                    seen.append(u)#add u to array of vertices seen
+                while u in tosee:
+                    tosee.pop(0)
+                    #tosee.pop(tosee.index(u))#remove seen vertice from list
+
+            #print(tosee)
+        # for each edge (u,v) for all G.E
+        for i in np.arange(len(G.E)-1):
+            uv = G.E[i]#uv is the from and to indice of edges
+            u = uv[0]
+            v = uv[1]
+            print(' u=' + str(u)+' v='+str(v) + ' w=' + str(w[i]) + ' v.d=' + str(G.V[v].d) + ' u.d=' + str(G.V[u].d))
+
+            if G.V[u].d > G.V[v].d + w[i]:#The addition of this u to v would decrease the current v.d
+                if uv[1] == s or uv[0] == s:
+                    return False, G#A negative cycle was found
+        return True, G#A negative cycle was not found
+
+    def relax(self,G,u,v,w):
+        """Checks to see if the suggested path would be shorted
+        G is the Graph
+        u - starting vertice
+        v - ending vertice
+        w - weight for u to v
+        """
+        if G.V[v].d > G.V[u].d + w:  # The distance from starting node s to current node v is longer than s to u + u to v
+            G.V[v].d = G.V[u].d + w  # replace s to v with s to u + u to v
+            G.V[v].pi = G.V[u] # update v's predecessor attribute pi
+        return G
+
--- homework2/bellmanFord/runBF.py
+++ homework2/bellmanFord/runBF.py
@@ -6,30 +6,44 @@ from bellmanFord import graph
 
 bF = bellmanFord()#create Instance
 
+###############################################################
 #Create Sample Directed Graph with No Negative Loops
-VnoNegLoops = range(0,4)
+VnoNegLoops = np.arange(0,5)
 EnoNegLoops = np.array([    [0,1,1,0,0],
                             [0,0,1,0,0],
                             [0,0,0,1,0],
                             [0,1,0,0,1],
                             [1,0,0,0,0]])
+#Parse out i to j connections taht exist nonzero edges
+EnoNegList = [[i,j] for i in np.arange(len(EnoNegLoops[0,:])) for j in np.arange(len(EnoNegLoops[:,0])) if abs(EnoNegLoops[i,j]) > 0]
+#Parse out weights
+w = np.zeros(len(EnoNegList))
+for i in np.arange(len(EnoNegList)):
+    w[i] = EnoNegLoops[EnoNegList[i][0],EnoNegList[i][1]]
 #no negative loops exist
+GnoNegLoops = graph(VnoNegLoops,EnoNegList,w)  # Create graph 
+LB, G = bF.bellman_ford(GnoNegLoops,0)
 
-#Create Sample Directed Graph with Negative Loops
-VNegLoops = range(0,4)
-ENegLoops = np.array([    [0,1,1,0,0],
-                            [0,0,1,0,0],
-                            [0,0,0,-1,0],
-                            [0,-1,0,0,1],
-                            [1,0,0,0,0]])
+assert LB == True, 'Negative weight cycles were found'
 
+################################################################
+# Create Sample Directed Graph with Negative Loops
+VNegLoops = np.arange(0,5)
+ENegLoops = np.array([  [0,1,1,0,0],
+                        [0,0,-1,0,0],
+                        [0,0,0,-1,0],
+                        [0,-1,0,0,1],
+                        [1,0,0,0,0]])
+#Parse out i to j connections that exist with nonzero weight
+ENegList = [[i,j] for i in np.arange(len(ENegLoops[0,:])) for j in np.arange(len(ENegLoops[:,0])) if abs(ENegLoops[i,j]) > 0]
+#Parse out weights
+w = np.zeros(len(ENegList))
+for i in np.arange(len(ENegList)):
+    w[i] = ENegLoops[ENegList[i][0],ENegList[i][1]]
 #1,2,3 is a negative loop
-
-#Create Graph
-GnoNegLoops = graph(VnoNegLoops,EnoNegLoops)
-GNegLoops = graph(VNegLoops,ENegLoops)
-
-
+GNegLoops = graph(VNegLoops,ENegList,w)
+LB, G = bF.bellman_ford(GNegLoops,0)
+assert LB == False, 'Negative weight cycles were NOT found'
 
 #If we were to randomly generate a graph, I would need to check...
 #1. Each node has a vector leaving itself and going to another node (absolute value of row sum >= 1)
