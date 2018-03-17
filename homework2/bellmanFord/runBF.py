c7e5285fffb90f7a6dc55c2030e61d2df317e873
Dean Keithly <drk94@cornell.edu>
2018-03-17 06:16:11 -0400

    bellmanFord tests running

--- homework2/bellmanFord/runBF.py
+++ homework2/bellmanFord/runBF.py
@@ -1,6 +1,5 @@
 import numpy as np
 import random
-
 from bellmanFord import bellmanFord
 from bellmanFord import graph
 
--- homework2/tests/test_bellmanFord.py
+++ homework2/tests/test_bellmanFord.py
@@ -1,10 +1,10 @@
 import unittest
 import hmwk2PATHS  # from hybridSort.hybridSort import hybridSort
-from hybridSort import runHS
+from bellmanFord import runBF
 hmwk2PATHS
 
 
-class test_hybridSort(unittest.TestCase):
+class test_bellmanFord(unittest.TestCase):
 
     def setUp(self):  # Generate Tree Randomly for each case
         pass
@@ -12,8 +12,8 @@ class test_hybridSort(unittest.TestCase):
     # def test_runHS(self):
     #     hS = hybridSort()
 
-    def test_runHS(self):
-        runHS
+    def test_runBF(self):
+        runBF
 
 
 if __name__ == '__main__':
