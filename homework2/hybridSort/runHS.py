58995df3fdfccb9880644ea3d8f3e1a17f8c028b
Dean Keithly <drk94@cornell.edu>
2018-03-16 22:28:31 -0400

    aded __init__.py to several subfolders

--- homework2/hybridSort/runHS.py
+++ homework2/hybridSort/runHS.py
@@ -1,8 +1,7 @@
 import numpy as np
 import random
 import hmwk2PATHS
-print(sys.path)
-from homework2.hybridSort import hybridSort
+from hybridSort import hybridSort
 hmwk2PATHS
 # import matplotlib.pyplot as plt
 
