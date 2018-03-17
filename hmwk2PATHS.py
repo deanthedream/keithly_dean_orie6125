6a8fb289d49df26eacb4a9c25820ae57e910c02c
Dean Keithly <drk94@cornell.edu>
2018-03-16 21:42:00 -0400

    added hmwk2PATHS to top most folder

---
+++ hmwk2PATHS.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import re
+import fnmatch
+#fnmatch.filter(os.listdir('.'), )
+files = [f for f in os.listdir('.') if os.path.isdir(f)] # Get dir in current dir
+files2 = [f for f in files if not '.' in f] # remove filename if there is a dot in it
+files3 = list()
+folderExclusion = ['tests','env','coverage','htmlcov']
+for folderName in files2:
+    if not folderName in folderExclusion:
+        files3.append(folderName)
+        sys.path.append(os.path.join(os.getcwd()+'/homework2/'+folderName))
+sys.path.append(os.path.join(os.getcwd()))
+
